29887e6c2ea022dd1087bf0a31f70235108bedd1
Tiara <tiaralaila21@students.unnes.ac.id>
2025-07-18 16:02:48 +0700

    first commit

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Buku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // menampilkan data
+    public function index()
+    {
+        $data = Buku::orderBy('judul', 'asc')->get();
+        return response()->json([
+            'status'=>true,
+            'message'=>'Data ditemukan',
+            'data'=>$data
+        ],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    // menyimpan data
+    public function store(Request $request)
+    {
+        $dataBuku = new Buku;
+
+        $rules = [
+            'judul'=>'required',
+            'pengarang'=>'required',
+            'tanggal_publikasi'=>'required|date',
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Gagal memasukan data',
+                'data'=>$validator->errors()
+
+            ]);
+        };
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang=$request->pengarang;
+        $dataBuku->tanggal_publikasi=$request->tanggal_publikasi;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status' => true,
+            'message'=>'Sukses memasukan data'
+
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    // mengambil data menurut parameter tertentu
+    public function show(string $id)
+    {
+        $data = Buku::find($id);
+        if($data){
+            return response()->json([
+                'status'=>true,
+                'message'=>'Data ditemukan',
+                'data'=>$data
+
+            ],200);
+        }else{
+            return response()->json([
+                'status'=>false,
+                'message'=>'Data tidak ditemukan'
+            ]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
