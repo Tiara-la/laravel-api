6f529ed67f8e31cfa2c015dea7c7a35f60186623
Tiara <tiaralaila21@students.unnes.ac.id>
2025-07-18 16:26:56 +0700

    create update destroy

--- app/Http/Controllers/Api/BukuController.php
+++ app/Http/Controllers/Api/BukuController.php
@@ -86,7 +86,41 @@ class BukuController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $dataBuku = Buku::find($id);
+        if(empty($dataBuku)){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Data tidak ditemukan'
+            ],404);
+        };
+
+        $rules = [
+            'judul'=>'required',
+            'pengarang'=>'required',
+            'tanggal_publikasi'=>'required|date',
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Gagal melakukan update data',
+                'data'=>$validator->errors()
+
+            ]);
+        }
+
+        $dataBuku->judul = $request->judul;
+        $dataBuku->pengarang=$request->pengarang;
+        $dataBuku->tanggal_publikasi=$request->tanggal_publikasi;
+
+        $post = $dataBuku->save();
+
+        return response()->json([
+            'status' => true,
+            'message'=>'Sukses melakukan update data'
+
+        ]);
+        
     }
 
     /**
@@ -94,6 +128,20 @@ class BukuController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         $dataBuku = Buku::find($id);
+        if(empty($dataBuku)){
+            return response()->json([
+                'status'=>false,
+                'message'=>'Data tidak ditemukan'
+            ],404);
+        };
+
+        $post = $dataBuku->delete();
+
+        return response()->json([
+            'status' => true,
+            'message'=>'Sukses melakukan hapus data'
+
+        ]);
     }
 }
